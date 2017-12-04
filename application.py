fa79c17437ea98285be15f8091c45b8dd4e1161f
LEON-MING <leonming12@yahoo.com>
2017-12-04 02:11:11 -0800

    change flask-api to flask

--- application.py
+++ application.py
@@ -1,9 +1,9 @@
-from flask import request, url_for, make_response
+from flask import Flask, request, url_for, make_response, jsonify
 from flask_api import FlaskAPI, status, exceptions
 from flask_cors import CORS
 from zimmerbot import *
 
-application = FlaskAPI(__name__)
+application = Flask(__name__)
 CORS(application)
 
 @application.route("/", methods=["GET", "POST"])
@@ -14,7 +14,8 @@ def get_links():
         print(request)
         data = request.data
         list_of_links = main(data["query"], data["language"], data["filter"], data["limit"])
-    return list_of_links
+    return jsonify(list_of_links)
+
 
 # run the app.
 if __name__ == "__main__":
