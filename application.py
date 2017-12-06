bac7b3142edf25d9f042d9d27278cf22057ee000
LEON-MING <leonming12@yahoo.com>
2017-12-06 04:03:05 -0800

    changed error code

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import request, url_for, make_response, jsonify
+from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 from flask_cors import CORS
 from zimmerbot import *
@@ -14,10 +14,10 @@ def get_links():
         print(request.data)
         data = request.data
         if data["filter"] == "ores_quality" and data["language"] not in ["en", "ru", "fr"]:
-            return ["ORES is not supported in this language"], status.HTTP_204_NO_CONTENT
+            return ["ORES is not supported in this language"], status.HTTP_202_ACCEPTED
         list_of_links = main(data["query"], data["language"], data["filter"], data["limit"], data["stub"])
         if not list_of_links:
-            return ["No search results found for this query"], status.HTTP_204_NO_CONTENT
+            return ["No search results found for this query"], status.HTTP_202_ACCEPTED
     return list_of_links
 
 
